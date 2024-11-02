2973c711549b7f51d7885487da8119b26ae4aa34
Felipe Campos <felipecampos50123@gmail.com>
2024-11-02 12:26:16 -0300

    .

--- src/fuzzy_relation_4.py
+++ src/fuzzy_relation_4.py
@@ -2,10 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from membership_functions_1 import triangular
-from fuzzy_operations_3 import fuzzy_t_norm, fuzzy_s_norm
+from fuzzy_operations_3 import (
+    t_norm_min,
+    t_norm_product,
+    t_norm_limited_product,
+    t_norm_drastic_product,
+)
 
 
-def fuzzy_relation(set1, set2, t_norm):
+def fuzzy_relation(set1, set2, operator):
     if len(set1) != len(set2):
         raise ValueError("Os conjuntos devem ter o mesmo tamanho.")
 
@@ -13,29 +18,29 @@ def fuzzy_relation(set1, set2, t_norm):
 
     for i in range(len(set1)):
         for j in range(len(set2)):
-            relation_matrix[i, j] = t_norm(set1[i], set2[j])
+            relation_matrix[i, j] = operator(set1[i], set2[j])
 
     return relation_matrix
 
 
-def plot_fuzzy_relations(set1, set2):
-    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+def plot_fuzzy_relations(set1, set2, operator):
+    relation = fuzzy_relation(set1, set2, operator)
 
-    relation_t_norm_product = fuzzy_relation(set1, set2, fuzzy_t_norm)
-    cax1 = axs[0].matshow(relation_t_norm_product, cmap="Blues")
-    fig.colorbar(cax1, ax=axs[0])
-    axs[0].set_title("T-norma (Product)")
-
-    relation_s_norm_minimum = fuzzy_relation(set1, set2, fuzzy_s_norm)
-    cax2 = axs[1].matshow(relation_s_norm_minimum, cmap="Blues")
-    fig.colorbar(cax2, ax=axs[1])
-    axs[1].set_title("S-norma (Minimum)")
+    fig, ax = plt.subplots(figsize=(6, 5))
+    cax = ax.matshow(relation, cmap="Blues")
+    fig.colorbar(cax, ax=ax)
+    ax.set_title(f"Relação Fuzzy usando {operator.__name__}")
 
     plt.show()
 
 
-####### Exemplos de execução
-universe = np.linspace(0, 50, 100)
+# Exemplo de execução
+universe = np.linspace(0, 50, 10)
 
-# Plot dos resultados
-plot_fuzzy_relations(triangular(universe, -2, 5, 10), triangular(universe, 7.5, 15, 20))
+set1 = triangular(universe, -2, 5, 10)
+set2 = triangular(universe, 20, 30, 40)
+
+plot_fuzzy_relations(set1, set2, t_norm_min)
+plot_fuzzy_relations(set1, set2, t_norm_product)
+plot_fuzzy_relations(set1, set2, t_norm_drastic_product)
+plot_fuzzy_relations(set1, set2, t_norm_limited_product)
